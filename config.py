98148f70aae0225c1adceb32b6cfc071b6e8173f
= <=>
2025-02-20 00:41:05 +0530

    removed env vars

--- config.py
+++ config.py
@@ -3,21 +3,20 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config (object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-guess-it'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-guess-it-yes'
     
     # database config
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'movies.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # OMDB API
-    OMDB_API_KEY = os.environ.get('OMDB_API_KEY') or "28c6a732"
-    OMDB_URL = os.environ.get('OMDB_URL') or "http://www.omdbapi.com/"
-
+    OMDB_API_KEY = os.environ.get('OMDB_API_KEY')
+    OMDB_URL = os.environ.get('OMDB_URL') 
     # Caching Redis
     CACHE_TYPE = os.environ.get('CACHE_TYPE') or "RedisCache"
-    CACHE_REDIS_HOST = os.environ.get('CACHE_REDIS_HOST') or "redis-17128.crce182.ap-south-1-1.ec2.redns.redis-cloud.com"
+    CACHE_REDIS_HOST = os.environ.get('CACHE_REDIS_HOST')  
     CACHE_REDIS_PORT = os.environ.get('CACHE_REDIS_PORT') or 17128
-    CACHE_REDIS_PASSWORD = os.environ.get('CACHE_REDIS_PASSWORD') or "N7KCwAsyZfeZlcTHWtyLIBDaMl3DMTs9"
+    CACHE_REDIS_PASSWORD = os.environ.get('CACHE_REDIS_PASSWORD') 
     # CACHE_REDIS_USERNAME = os.environ.get('CACHE_REDIS_PASSWORD') or "default"
     CACHE_DEFAULT_TIMEOUT = os.environ.get('CACHE_DEFAULT_TIMEOUT') or 600
 
@@ -27,6 +26,6 @@ class Config (object):
 
 
     # Qdrant API
-    QDRANT_API_KEY = os.environ.get('QDRANT_KEY') or "R0sgiW6pakm1ZnSqy7oWVYEhbzXpT1EUCQ16GoSNVKmjyV-oyLU2XQ"
-    QDRANT_URL = os.environ.get('QDRANT_URL') or "https://5c4686a7-f60e-445b-81c2-b235697b9d42.europe-west3-0.gcp.cloud.qdrant.io:6333"
-    QDRANT_COLLECTION_NAME = os.environ.get('QDRANT_COLLECTION_NAME') or 'recommendation'
+    QDRANT_API_KEY = os.environ.get('QDRANT_KEY') 
+    QDRANT_URL = os.environ.get('QDRANT_URL')
+    QDRANT_COLLECTION_NAME = os.environ.get('QDRANT_COLLECTION_NAME')
--- populate_data.py
+++ populate_data.py
@@ -1,21 +1,106 @@
-from app import app,db
-from app.models import Movie, TopMovie
+# from app import app,db
+# from app.models import Movie, TopMovie
+# import pandas as pd
+
+# movies = pd.read_csv('../movies.csv')
+# top_movies = pd.read_csv('../top_movies.csv')
+
+
+# with app.app_context():
+#     for _, row in movies.iterrows():
+#         movie = Movie(id=row['id'],title=row['title'])
+#         db.session.add(movie)
+#     db.session.commit()
+# print('1 done')
+# with app.app_context():
+#     for _, row in top_movies.iterrows():
+#         movie = TopMovie(id=row['id'],title=row['title'])
+#         db.session.add(movie)
+#     db.session.commit()
+
+# print('2 done')
+
+
 import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
-movies = pd.read_csv('../movies.csv')
-top_movies = pd.read_csv('../top_movies.csv')
+# Replace this with your actual PostgreSQL database URL
+DATABASE_URL = 
 
+# Create database engine
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
 
-with app.app_context():
+# Define tables
+movie_table = Table(
+    "movie", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String, nullable=False)
+)
+
+top_movie_table = Table(
+    "top_movie", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String, nullable=False)
+)
+
+# Create tables in PostgreSQL
+metadata.create_all(engine)
+
+# Load CSV files
+movies = pd.read_csv("../movies.csv")
+top_movies = pd.read_csv("../top_movies.csv")
+
+# Insert data into PostgreSQL using session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with engine.begin() as conn:
     for _, row in movies.iterrows():
-        movie = Movie(id=row['id'],title=row['title'])
-        db.session.add(movie)
-    db.session.commit()
-print('1 done')
-with app.app_context():
-    for _, row in top_movies.iterrows():
-        movie = TopMovie(id=row['id'],title=row['title'])
-        db.session.add(movie)
-    db.session.commit()
+        conn.execute(movie_table.insert().values(id=row['id'], title=row['title']))
+    print("Movies inserted.")
 
-print('2 done')
+    for _, row in top_movies.iterrows():
+        conn.execute(top_movie_table.insert().values(id=row['id'], title=row['title']))
+    print("Top Movies inserted.")
+
+
+# import pandas as pd
+# from sqlalchemy import create_engine, MetaData, Table
+# from sqlalchemy.orm import sessionmaker
+
+# # Replace this with your actual PostgreSQL database URL
+# DATABASE_URL = 
+# # Create database engine
+# engine = create_engine(DATABASE_URL)
+# metadata = MetaData(bind=engine)
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+# # Load CSV files
+# movies = pd.read_csv("../movies.csv")
+# top_movies = pd.read_csv("../top_movies.csv")
+
+# # Get the tables
+# movie_table = Table("movie", metadata, autoload_with=engine)
+# top_movie_table = Table("top_movie", metadata, autoload_with=engine)
+
+# # Insert data into movie table
+# with engine.connect() as conn:
+#     for _, row in movies.iterrows():
+#         conn.execute(movie_table.insert().values(id=row['id'], title=row['title']))
+#     conn.commit()
+
+# print("Movies inserted.")
+
+# # Insert data into top_movie table
+# with engine.connect() as conn:
+#     for _, row in top_movies.iterrows():
+#         conn.execute(top_movie_table.insert().values(id=row['id'], title=row['title']))
+#     conn.commit()
+
+# print("Top Movies inserted.")
+
+
+
